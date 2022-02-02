0d50bc2865f617fc3889611f75f2df3653aee51e
Vivekko <vivekojha961@gmail.com>
2022-02-02 15:45:38 +0530

    Push

---
+++ Level 1/Recursion using arraylist/GetStairPaths.cpp
@@ -0,0 +1,94 @@
+// 1. You are given a number n representing number of stairs in a staircase.
+// 2. You are standing at the bottom of staircase. You are allowed to climb 1 step, 2 steps or 3 steps in one move.
+// 3. Complete the body of getStairPaths function - without changing signature - to get the list of all paths that can be used to climb the staircase up.
+
+#include <bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;} 
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+#define NFS(nitro) ios_base::sync_with_stdio(false);cin.tie(0);
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+#define int long long
+#define vivek main
+#define all(x) (x).begin(),(x).end()
+#define vec_p vector<pair<int,int>
+#define v vector<int>
+
+vector<string> getStairPaths(int n){
+    if(n == 0){
+        vector<string> ret;
+        // ret.assign()
+        ret.push_back("");
+        return ret;
+    }
+    else if(n < 0){
+        vector<string> ret;
+        // ret.push_back("");
+        return ret;
+
+    }
+
+    vector<string> path1 = getStairPaths(n-1);
+    vector<string> path2 = getStairPaths(n-2);
+    vector<string> path3 = getStairPaths(n-3);
+    
+    vector<string> realans;
+
+    for(string str:path1){
+        realans.push_back(str+"1");
+    }
+    
+    for(string str:path2){
+        realans.push_back(str+"2");
+    }
+    
+    for(string str:path3){
+        realans.push_back(str+"3");
+    }
+    return realans;
+
+}
+
+
+void solve(){
+    int n;cin>>n;
+    vector<string> ret = getStairPaths(n);
+    debug(ret);
+}
+int32_t vivek(){
+NFS(NITRO);
+int t=1;
+// cin>>t;
+while(t-->0)
+    solve();
+return 0;
+}
+
+// ___.   .__                 __          .__              __    
+// \_ |__ |  | _____    ____ |  | _____  _|__|__  __ ____ |  | __
+//  | __ \|  | \__  \ _/ ___\|  |/ /\  \/ /  \  \/ // __ \|  |/ /
+//  | \_\ \  |__/ __ \   \___|    <  \   /|  |\   /\  ___/|    < 
+//  |___  /____(____  /\___  >__|_ \  \_/ |__| \_/  \___  >__|_ \
+//      \/          \/     \/     \/                    \/     \/
